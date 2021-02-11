d5f8b8d67a6980190b9814b90929e99fb2c7dcc7
Partner-project <76723144+Partner-project@users.noreply.github.com>
2021-02-11 19:41:11 +0900

    Delete File
    Delete File

--- TimerProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TimerProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TimerProject, "TimerProject" );
